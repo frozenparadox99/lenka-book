e8da3b2c5be344a71d5898b46cf4a928b8bc5323
frozenparadox99 <frostbitecryengine300@gmail.com>
2022-06-25 21:17:35 +0530

    render code in iframe

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
@@ -38,8 +39,24 @@ const App = () => {
 
     console.log(result);
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+  <html>
+    <head></head>
+    <body>
+        <div id="root" >
+            <script>
+                window.addEventListener('message',(event)=>{
+                    eval(event.data)
+                }, false);
+            </script>
+        </div>
+    </body>
+  </html>
+  `;
 
   return (
     <div>
@@ -51,6 +68,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html}></iframe>
     </div>
   );
 };

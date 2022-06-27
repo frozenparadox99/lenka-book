d6d0eb80d3d638ed395c83a48e6e4087b31c25d6
frozenparadox99 <frostbitecryengine300@gmail.com>
2022-06-27 15:23:17 +0530

    added code formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import "bulmaswatch/solar/bulmaswatch.min.css";
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
@@ -69,7 +70,10 @@ const App = () => {
 
   return (
     <div>
-      <CodeEditor initialValue="const a = 1;" />
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}

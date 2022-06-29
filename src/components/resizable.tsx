246090f368a1f3a09dfd25a9429a785508ee4d3f
frozenparadox99 <frostbitecryengine300@gmail.com>
2022-06-29 19:35:48 +0530

    horizontal scroll bug fixes

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,17 +1,34 @@
 import "./resizable.css";
 import React from "react";
-import { ResizableBox } from "react-resizable";
+import { ResizableBox, ResizableBoxProps } from "react-resizable";
 
 interface ResizableProps {
   direction: "horizontal" | "vertical";
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox resizeHandles={["s"]} height={300} width={Infinity}>
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === "horizontal") {
+    resizableProps = {
+      className: "resize-horizontal",
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      resizeHandles: ["e"],
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+    };
+  } else {
+    resizableProps = {
+      minConstraints: [Infinity, 30],
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      resizeHandles: ["s"],
+      height: 300,
+      width: Infinity,
+    };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;

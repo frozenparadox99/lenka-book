49c617286312f29336446bf4d703361c140ed49a
frozenparadox99 <frostbitecryengine300@gmail.com>
2022-07-06 15:06:17 +0530

    changed cell addition logic

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -45,7 +45,7 @@ const reducer = produce(
 
         return state;
 
-      case ActionType.INSERT_CELL_BEFORE:
+      case ActionType.INSERT_CELL_AFTER:
         const cell: Cell = {
           content: "",
           type: action.payload.type,
@@ -56,9 +56,9 @@ const reducer = produce(
 
         const index = state.order.findIndex((id) => id === action.payload.id);
         if (index < 0) {
-          state.order.push(cell.id);
+          state.order.unshift(cell.id);
         } else {
-          state.order.splice(index, 0, cell.id);
+          state.order.splice(index + 1, 0, cell.id);
         }
 
         return state;

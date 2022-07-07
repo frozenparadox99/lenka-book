ff12870a8bd3ce9fbced83f09943d9668c933d7e
frozenparadox99 <frostbitecryengine300@gmail.com>
2022-07-07 14:30:08 +0530

    added: progress bar

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,3 +1,4 @@
+import "./code-cell.css";
 import { useEffect } from "react";
 import { useActions } from "../hooks/use-actions";
 import { useTypedSelector } from "../hooks/use-typed-selector";
@@ -17,6 +18,11 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 1000);
@@ -41,7 +47,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview err={bundle.err} code={bundle.code} />}
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress max="100" className="progress is-small is-primary">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview err={bundle.err} code={bundle.code} />
+          )}
+        </div>
       </div>
     </Resizable>
   );

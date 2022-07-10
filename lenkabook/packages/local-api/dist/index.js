fb7a81075cbce2c5b330628ec631347d5beb62e9
frozenparadox99 <frostbitecryengine300@gmail.com>
2022-07-10 09:46:28 +0530

    added cli provisions for users to enter the path and port number

--- lenkabook/packages/local-api/dist/index.js
+++ lenkabook/packages/local-api/dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => {
-    console.log("Server is listening ....");
+exports.serve = void 0;
+const serve = (port, filename, dir) => {
+    console.log("serving traffic on port " + port);
+    console.log("filename " + filename);
+    console.log("path to file " + dir);
 };
+exports.serve = serve;
--- lenkabook/packages/local-api/src/index.ts
+++ lenkabook/packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-  console.log("Server is listening ....");
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log("serving traffic on port " + port);
+  console.log("filename " + filename);
+  console.log("path to file " + dir);
 };
